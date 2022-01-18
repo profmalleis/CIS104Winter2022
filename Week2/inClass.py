1bf134915cdb6c7331ab1aa1a9eaf14258e6bc71
profmalleis <mmalleis@ncmich.edu>
2022-01-18 18:51:18 -0500

    Add files via upload
    created week 2 folder

---
+++ Week2/inClass.py
@@ -0,0 +1,8 @@
+#print("Week 2 In Class assingment")
+hours = int(input("How many hours did you work? "))
+rate = int(input("What rate were you working at? "))
+total = hours * rate
+print("You made", total, "dollars")
+
+originalHours = total/rate
+print("original hours", originalHours)
